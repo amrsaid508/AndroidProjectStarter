b831401d4bee814031bdf43a15ce43dbc9507ba7
dhruv-russmediatech <dhruv.patel@russmediatech.in>
2015-05-19 12:21:49 +0530

    Removal of ImageTouchView project and added dependency. Resolved Null pointer issue on click on empty view recycler view.

--- project_starter/src/com/deardhruv/projectstarter/activities/MainActivity.java
+++ project_starter/src/com/deardhruv/projectstarter/activities/MainActivity.java
@@ -1,7 +1,15 @@
 
 package com.deardhruv.projectstarter.activities;
 
-import java.util.ArrayList;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.Button;
 
 import com.deardhruv.projectstarter.ProjectStarterApplication;
 import com.deardhruv.projectstarter.R;
@@ -17,15 +25,8 @@ import com.deardhruv.projectstarter.utils.Helper;
 import com.deardhruv.projectstarter.utils.Logger;
 import com.facebook.shimmer.ShimmerFrameLayout;
 
-import android.app.ProgressDialog;
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.Window;
-import android.widget.Button;
+import java.util.ArrayList;
+
 import de.greenrobot.event.EventBus;
 import jp.wasabeef.recyclerview.animators.FadeInAnimator;
 import jp.wasabeef.recyclerview.animators.adapters.AlphaInAnimationAdapter;
@@ -33,184 +34,186 @@ import jp.wasabeef.recyclerview.animators.adapters.ScaleInAnimationAdapter;
 
 public class MainActivity extends AbstractActivity implements OnClickListener {
 
-	private static final String LOGTAG = "MainActivity";
-	private static final Logger LOG = new Logger(LOGTAG);
+    private static final String LOGTAG = "MainActivity";
+    private static final Logger LOG = new Logger(LOGTAG);
 
-	private static final String IMAGE_LIST_REQUEST_TAG = LOGTAG + ".imageListRequest";
+    private static final String IMAGE_LIST_REQUEST_TAG = LOGTAG + ".imageListRequest";
 
-	private EventBus mEventBus;
-	private ApiClient mApiClient;
+    private EventBus mEventBus;
+    private ApiClient mApiClient;
 
-	private Button btnReload, btnUploadFile;
-	private ProgressDialog pd;
-	private RecyclerView recyclerView;
-	private ShimmerFrameLayout shimmerFrameLayout;
+    private Button btnReload, btnUploadFile;
+    private ProgressDialog pd;
+    private RecyclerView recyclerView;
+    private ShimmerFrameLayout shimmerFrameLayout;
 
-	private ArrayList<String> mImageUrls;
+    private ArrayList<String> mImageUrls;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
-		setProgressBarIndeterminateVisibility(true);
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+        setProgressBarIndeterminateVisibility(true);
 
-		Helper.riseAndShine(this);
+        Helper.riseAndShine(this);
 
-		setContentView(R.layout.main_activity_layout);
-		initUI();
+        setContentView(R.layout.main_activity_layout);
+        initUI();
 
-		mEventBus = EventBus.getDefault();
-		ProjectStarterApplication app = ((ProjectStarterApplication) getApplication());
-		mApiClient = app.getApiClient();
+        mEventBus = EventBus.getDefault();
+        ProjectStarterApplication app = ((ProjectStarterApplication) getApplication());
+        mApiClient = app.getApiClient();
 
-	}
+    }
 
-	private void initUI() {
-		btnReload = (Button) findViewById(R.id.btnReload);
-		btnUploadFile = (Button) findViewById(R.id.btnUploadFile);
-		recyclerView = (RecyclerView) findViewById(R.id.listPhotos);
+    private void initUI() {
+        btnReload = (Button) findViewById(R.id.btnReload);
+        btnUploadFile = (Button) findViewById(R.id.btnUploadFile);
+        recyclerView = (RecyclerView) findViewById(R.id.listPhotos);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
 
-		shimmerFrameLayout = (ShimmerFrameLayout) findViewById(R.id.shimmer_view_container);
-		shimmerFrameLayout.startShimmerAnimation();
+        shimmerFrameLayout = (ShimmerFrameLayout) findViewById(R.id.shimmer_view_container);
+        shimmerFrameLayout.startShimmerAnimation();
 
-		initListener();
-	}
+        initListener();
+    }
 
-	private void initListener() {
-		btnReload.setOnClickListener(this);
-		btnUploadFile.setOnClickListener(this);
-	}
+    private void initListener() {
+        btnReload.setOnClickListener(this);
+        btnUploadFile.setOnClickListener(this);
+    }
 
-	@Override
-	protected void onResume() {
-		super.onResume();
-		mEventBus.register(this);
-	}
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mEventBus.register(this);
+    }
 
-	@Override
-	protected void onPause() {
-		super.onPause();
-		mEventBus.unregister(this);
-	}
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mEventBus.unregister(this);
+    }
 
-	private void loadImages() {
-		showProgressDialog();
-		mApiClient.getImageList(IMAGE_LIST_REQUEST_TAG);
-	}
+    private void loadImages() {
+        showProgressDialog();
+        mApiClient.getImageList(IMAGE_LIST_REQUEST_TAG);
+    }
 
-	private void showProgressDialog() {
-		btnReload.setEnabled(false);
-		pd = ProgressDialog.show(MainActivity.this, "Please wait", "getting images...");
-		if (!pd.isShowing()) {
-			pd.show();
-		}
-		setProgressBarIndeterminateVisibility(true);
-	}
+    private void showProgressDialog() {
+        btnReload.setEnabled(false);
+        pd = ProgressDialog.show(MainActivity.this, "Please wait", "getting images...");
+        if (!pd.isShowing()) {
+            pd.show();
+        }
+        setProgressBarIndeterminateVisibility(true);
+    }
 
-	private void dismissProgressDialog() {
-		btnReload.setEnabled(true);
-		if (pd.isShowing()) {
-			pd.dismiss();
-		}
-		setProgressBarIndeterminateVisibility(false);
-	}
+    private void dismissProgressDialog() {
+        btnReload.setEnabled(true);
+        if (pd.isShowing()) {
+            pd.dismiss();
+        }
+        setProgressBarIndeterminateVisibility(false);
+    }
 
-	// ============================================================================================
-	// User Clicks and Actions
-	// ============================================================================================
+    // ============================================================================================
+    // User Clicks and Actions
+    // ============================================================================================
 
-	@Override
-	public void onClick(View v) {
-		switch (v.getId()) {
-			case R.id.btnReload:
-				loadImages();
-				break;
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnReload:
+                loadImages();
+                break;
 
-			case R.id.btnUploadFile:
-				final Intent intent = new Intent(MainActivity.this, UploadFileActivity.class);
-				startActivity(intent);
-				break;
+            case R.id.btnUploadFile:
+                final Intent intent = new Intent(MainActivity.this, UploadFileActivity.class);
+                startActivity(intent);
+                break;
 
-			default:
-				LOG.i("default case");
-				break;
-		}
-	}
+            default:
+                LOG.i("default case");
+                break;
+        }
+    }
 
-	// ============================================================================================
-	// EventBus callbacks
-	// ============================================================================================
+    // ============================================================================================
+    // EventBus callbacks
+    // ============================================================================================
 
-	/**
-	 * Response of Image list.
-	 * 
-	 * @param imageListResponse ImageListResponse
-	 */
-	public void onEventMainThread(ImageListResponse imageListResponse) {
-		switch (imageListResponse.getRequestTag()) {
-			case IMAGE_LIST_REQUEST_TAG:
-				dismissProgressDialog();
-				ImageItemDetailAdapter adapter = new ImageItemDetailAdapter(MainActivity.this,
-						imageListResponse.getData().getImageResultList());
-				// listPhotos.setAdapter(adapter);
+    /**
+     * Response of Image list.
+     *
+     * @param imageListResponse ImageListResponse
+     */
+    public void onEventMainThread(ImageListResponse imageListResponse) {
+        switch (imageListResponse.getRequestTag()) {
+            case IMAGE_LIST_REQUEST_TAG:
+                dismissProgressDialog();
+                ImageItemDetailAdapter adapter = new ImageItemDetailAdapter(MainActivity.this,
+                        imageListResponse.getData().getImageResultList());
+                // listPhotos.setAdapter(adapter);
 
-				recyclerView.setLayoutManager(new LinearLayoutManager(this));
-				recyclerView.setItemAnimator(new FadeInAnimator());
+                recyclerView.setLayoutManager(new LinearLayoutManager(this));
+                recyclerView.setItemAnimator(new FadeInAnimator());
 
-				adapter.setRecyclerView(recyclerView);
+                adapter.setRecyclerView(recyclerView);
 
-				AlphaInAnimationAdapter alphaAdapter = new AlphaInAnimationAdapter(adapter);
-				ScaleInAnimationAdapter scaleAdapter = new ScaleInAnimationAdapter(alphaAdapter);
-				// scaleAdapter.setFirstOnly(false);
-				// scaleAdapter.setInterpolator(new OvershootInterpolator());
-				recyclerView.setAdapter(scaleAdapter);
+                AlphaInAnimationAdapter alphaAdapter = new AlphaInAnimationAdapter(adapter);
+                ScaleInAnimationAdapter scaleAdapter = new ScaleInAnimationAdapter(alphaAdapter);
+                // scaleAdapter.setFirstOnly(false);
+                // scaleAdapter.setInterpolator(new OvershootInterpolator());
+                recyclerView.setAdapter(scaleAdapter);
 
-				mImageUrls = new ArrayList<>();
-				for (ImageResult imageResult : imageListResponse.getData().getImageResultList()) {
-					mImageUrls.add(imageResult.getImg());
-				}
+                mImageUrls = new ArrayList<>();
+                for (ImageResult imageResult : imageListResponse.getData().getImageResultList()) {
+                    mImageUrls.add(imageResult.getImg());
+                }
 
-				Dumper.dump(imageListResponse);
-				break;
+                Dumper.dump(imageListResponse);
+                break;
 
-			default:
-				break;
-		}
-	}
+            default:
+                break;
+        }
+    }
 
-	/**
-	 * EventBus listener. An API call failed. No error message was returned.
-	 *
-	 * @param event ApiErrorEvent
-	 */
-	public void onEventMainThread(ApiErrorEvent event) {
-		switch (event.getRequestTag()) {
-			case IMAGE_LIST_REQUEST_TAG:
-				dismissProgressDialog();
-				showToast(getString(R.string.error_server_problem));
-				break;
+    /**
+     * EventBus listener. An API call failed. No error message was returned.
+     *
+     * @param event ApiErrorEvent
+     */
+    public void onEventMainThread(ApiErrorEvent event) {
+        switch (event.getRequestTag()) {
+            case IMAGE_LIST_REQUEST_TAG:
+                dismissProgressDialog();
+                showToast(getString(R.string.error_server_problem));
+                break;
 
-			default:
-				break;
-		}
-	}
+            default:
+                break;
+        }
+    }
 
-	/**
-	 * EventBus listener. An API call failed. An error message was returned.
-	 *
-	 * @param event ApiErrorWithMessageEvent Contains the error message.
-	 */
-	public void onEventMainThread(ApiErrorWithMessageEvent event) {
-		switch (event.getRequestTag()) {
-			case IMAGE_LIST_REQUEST_TAG:
-				dismissProgressDialog();
-				showToast(event.getResultMsgUser());
-				break;
+    /**
+     * EventBus listener. An API call failed. An error message was returned.
+     *
+     * @param event ApiErrorWithMessageEvent Contains the error message.
+     */
+    public void onEventMainThread(ApiErrorWithMessageEvent event) {
+        switch (event.getRequestTag()) {
+            case IMAGE_LIST_REQUEST_TAG:
+                dismissProgressDialog();
+                showToast(event.getResultMsgUser());
+                break;
 
-			default:
-				break;
-		}
-	}
+            default:
+                break;
+        }
+    }
 
 }
